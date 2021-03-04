8fd779ad16d23f0cc650192cb75a776ce6417829
babas2 <79476483+babas2@users.noreply.github.com>
2021-03-04 17:01:53 +0400

    Update he.py

--- he.py
+++ he.py
@@ -5,8 +5,11 @@
 
 import random
 
+a1 = {"sual": "Babaş Necə İnsandı?", "cvb": "mükəmməl"}
+a2 = {"sual": "Nurlan Kimdi", "cvb': "alverçi"}
 
-suallar = open("sozler.txt", "r")
+suallar = a1, a2
+# suallar = open("sozler.txt", "r")
 random.choice(suallar)
 
 

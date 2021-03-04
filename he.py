19ef84001be771aa68a64df4b8302f7421045e2a
babas2 <79476483+babas2@users.noreply.github.com>
2021-03-04 16:55:47 +0400

    Add files via upload

---
+++ he.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+import random
+
+
+
+
+suallar = {"sual":"Mən Kiməm?", "cvb":"babaş"}
+
+
+
+a = input((suallar["sual"]))
+
+if a == suallar["cvb"]:
+  print("doğru")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
